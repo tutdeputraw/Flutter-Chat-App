c3b45888a0b35a29feee3bcc1a3578ce09b35513
tutdeputraw <anoboy.code@gmail.com>
2021-10-30 13:46:36 +0700

    first commit

---
+++ lib/ui/screens/screens.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+part 'sign_in/sign_in.dart';
---
+++ lib/ui/screens/sign_in/sign_in.dart
@@ -0,0 +1,12 @@
+part of '../../screens/screens.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('tes'),
+    );
+  }
+}
